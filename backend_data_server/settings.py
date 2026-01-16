5e4527800579a84ba15f5000b9c533e18fcd7914
Marlo Valle <152346851+MarSalvatore28@users.noreply.github.com>
2026-01-16 04:42:01 +0000

    trabajo guia 20 listo

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,10 +56,11 @@ ROOT_URLCONF = "backend_data_server.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -67,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = "backend_data_server.wsgi.application"
 
@@ -117,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, STATIC_URL),
+]
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("¡Bienvenido a la aplicación Django!")
+    # return HttpResponse("¡Bienvenido a la aplicación Django!")
+    return render(request, 'homepage/index.html')
